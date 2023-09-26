5573df2bfd476c883a7f812dfd408533db44ea4d
jestebanpelaez18 <juanes.pelaez18@gmail.com>
2023-09-26 18:22:02 +0300

    cpp1

---
+++ cpp1/ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 18:13:37 by jpelaez-          #+#    #+#             */
+/*   Updated: 2023/09/26 18:17:43 by jpelaez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+    private:
+
+    std:: string name;
+    void announce(void);
+    
+};
+
+#endif
