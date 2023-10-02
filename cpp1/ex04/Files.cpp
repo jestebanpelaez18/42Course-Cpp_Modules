a7aa452023ca16668a18e0a15d4515b6718c32ec
jestebanpelaez18 <juanes.pelaez18@gmail.com>
2023-10-02 18:17:47 +0300

    working in ex04

--- cpp1/ex04/Files.cpp
+++ cpp1/ex04/Files.cpp
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 17:09:47 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/10/02 17:20:22 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/10/02 18:15:00 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void file_replace(std::ifstream& file, std::string s1, std::string s2)
 {
+    std::ifstream file("test1");
+    std::ifstream outputfile("test1.compare");
+
+    if(file.is_open())
+    {
+        while(std::getline(file, Files::line))
+        {
+            
+        }
+        
+    }
     
 }
--- cpp1/ex04/Files.hpp
+++ cpp1/ex04/Files.hpp
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 17:09:56 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/10/02 17:19:56 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/10/02 18:13:53 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 class Files
 {
     private:
+    std:: string line;
 
     public:
     void file_replace(std::ifstream& file, std::string s1, std::string s2);
