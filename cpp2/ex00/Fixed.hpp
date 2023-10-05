179b435a6c1135ff3b4437eabd0d50a7784020cc
jestebanpelaez18 <juanes.pelaez18@gmail.com>
2023-10-05 20:00:19 +0300

    ex00 cpp2

--- cpp2/ex00/Fixed.hpp
+++ cpp2/ex00/Fixed.hpp
@@ -6,7 +6,29 @@
 /*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 14:23:07 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/10/04 14:23:07 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/10/05 19:07:15 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed 
+{
+    private:
+    int fixed_point_number;
+    static const int number_fractional_bits = 8;
+    
+    public:
+    Fixed();
+    ~Fixed();
+    Fixed& operator=(Fixed const & fp);
+    Fixed(const Fixed & fp);
+    int getRawBits(void)const;
+    void setRawBits(int const raw);
+    
+};
+
+#endif
--- cpp2/ex00/main.cpp
+++ cpp2/ex00/main.cpp
@@ -6,7 +6,20 @@
 /*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 14:22:40 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/10/04 14:22:42 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/10/05 17:23:57 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int main(void)
+{
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
