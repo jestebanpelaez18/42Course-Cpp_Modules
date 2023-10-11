ec067bae2b200c25510c7c7c98a246b2264aa780
jestebanpelaez18 <juanes.pelaez18@gmail.com>
2023-10-11 16:28:57 +0300

    working in cpp3

--- cpp3/ex00/ClapTrap.hpp
+++ cpp3/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:43:43 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/10/10 16:47:50 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:45:51 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,11 @@ class ClapTrap
     int Attack_damage;
     
     public:
+    
     ClapTrap(std:: string name);
     ~ClapTrap();
+    ClapTrap& operator=(ClapTrap const & fp);
+    ClapTrap(const ClapTrap & fp);
     void attack(const std::string& target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
