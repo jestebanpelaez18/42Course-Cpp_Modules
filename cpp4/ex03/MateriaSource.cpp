7b19148d7ecc712f48af232a6b7889ce29b16960
jestebanpelaez18 <juanes.pelaez18@gmail.com>
2023-10-18 18:28:31 +0300

    all the stuff for cpp4 created

--- cpp4/ex03/MateriaSource.cpp
+++ cpp4/ex03/MateriaSource.cpp
@@ -1,12 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   IMateriaSource.cpp                                 :+:      :+:    :+:   */
+/*   MateriaSource.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 17:54:03 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/10/18 17:54:03 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/10/18 18:27:31 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource()
+{
+    std::cout << "Default MateriaSource constructor called" << std::endl;
+}
+MateriaSource::~MateriaSource()
+{
+    std::cout << "Default MateriaSource destructor called" << std::endl;
+}
+
+MateriaSource::MateriaSource(const MateriaSource & fp)
+{
+    // this->type = fp.type;
+    std::cout << "Copy MateriaSource constructor called" << std::endl;  
+}
+MateriaSource& MateriaSource::operator=(MateriaSource const & fp)
+{
+    // this->type = fp.type;
+    return *this;
+}
--- cpp4/ex03/MateriaSource.hpp
+++ cpp4/ex03/MateriaSource.hpp
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 17:57:19 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/10/18 18:00:18 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/10/18 18:27:46 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ class MateriaSource : public IMateriaSource
     ~MateriaSource();
     MateriaSource& operator=(MateriaSource const & fp);
     MateriaSource(const MateriaSource & fp);
-    ~IMateriaSource() {}
     void learnMateria(AMateria*);
     AMateria* createMateria(std::string const & type);
 };
