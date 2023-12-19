24015ff2a69a847fa26a652dee7f7ff94e5f341f
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2023-12-19 17:30:56 +0100

    cpp7 ex01 done

---
+++ cpp7/ex01/iter.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/19 18:08:55 by jpelaez-          #+#    #+#             */
+/*   Updated: 2023/12/19 18:25:53 by jpelaez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+void iter(T a[], size_t len, void (*f)(T &))
+{
+    for(int i = 0; i < len; i++)
+        f(a[i]);
+}
+
+template <typename T>
+void print(T object)
+{
+    std::cout << object << std::endl;
+}
+#endif
