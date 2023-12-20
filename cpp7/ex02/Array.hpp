dccb77173eac214868e04bcdd36e932f919bd188
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2023-12-20 12:20:42 +0200

    almost done ex02

--- cpp7/ex02/Array.hpp
+++ cpp7/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 18:33:29 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/12/20 11:54:53 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/12/20 12:19:06 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,33 @@ class Array
     public:
     Array(): array(new T[0]), size(0) {};
     Array(unsigned int n): array(new T[n]), size(n) {};
-    ~Array();
+    ~Array()
+    {
+        if(array)
+            delete[] array;
+    };
     Array& operator=(Array const & fp)
     {
-
+        size = fp.size;
+        delete[] array;
+        array = new T[size];
+        for (int i = 0; i < size; i++)
+            array[i] = fp.array[i];
     };
-    Array(const Array & fp);
+    Array(const Array & fp): size(fp.size), array(new T[size]);
+    {
+        for (int i = 0; i < size; i++)
+            array[i] = fp.array[i];
+    }
     unsigned int size()
     { return(size);};
-    class OutOfBounds: public std::exception
+    T& operator[](unsigned int i)
+    {
+        if(i < size)
+            throw OutOfBounds();
+        return(array[i]);
+    }
+    class OutOfBounds: public std::out_of_range
     {
         const char * what () const throw () {
         return "Index out of bounds";
