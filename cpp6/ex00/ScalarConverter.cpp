0e05ecf02dd5e9e3b4b50176abe5d59527146398
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2023-12-28 19:25:57 +0200

    fixing something cpp6 ex00

--- cpp6/ex00/ScalarConverter.cpp
+++ cpp6/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 14:48:55 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/12/11 20:16:48 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/12/28 19:25:22 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,19 +32,108 @@ ScalarConverter& ScalarConverter::operator=(ScalarConverter const & fp)
     return *this;
 }
 
-int ScalarConverter::is_int(std::string value)
+/*Printers*/
+
+void ScalarConverter::print_int()
+{
+    
+}
+
+void ScalarConverter::print_char()
+{
+    if(!std::isprint(c))
+        std::cout << "Non displayable" << std::endl;
+    else if()
+}
+
+/*Setters*/
+
+void ScalarConverter::set_int(std::string &value)
+{
+    int newInt = std::stoi(value);
+
+    c = static_cast<char>(newInt);
+    n = newInt;
+    f = static_cast<float>(newInt);
+    d = static_cast<double>(newInt);
+}
+
+void ScalarConverter::set_char(char value)
+{
+    c = value;
+    n = static_cast<int>(value);
+    f = static_cast<float>(value);
+    d = static_cast<double>(value);
+}
+void ScalarConverter::set_type(std::string &value)
+{
+    if(is_char(value))
+        type = CHAR;
+    else if(is_int(value))
+        type = INT;
+    
+}
+/*identificators*/
+
+bool ScalarConverter::is_int(std::string value)
 {
     int i = 0;
-    while(value[i])
+    int sing = 1;
+    int long result = 0;
+    while(std::isspace(value[i]))
+        i++;
+    if(value[i] == '-' || value[i] == '+')
     {
+		if (value[i] == '-')
+			sing = -1;
+        i++;
+    }
+    for(int j = i; j < value.length(); j++)
+    {
+        if(!std::isdigit(value[j]))
+            return false;
+        result = result * 10 + (value[i] - '0');
+		if (result > 2147483648 && sing == -1)
+        {
+            isImposible = true;
+			return false;
+        }
+        else if (result > 2147483647 && sing == 1)
+        {
+            isImposible = true;
+			return (-1);       
+        }
+    }
+    return true;
     
-    }
 }
-void ScalarConverter::convert(std::string value)
+
+bool ScalarConverter::is_char(std::string &character)
 {
-    ScalarConverter Conv;
-    if(Conv.is_int(value))
+    if(character.size() == 1 && !std::isdigit(character[0]) && std::isprint(character[0]))
+        return true;
+    return false;
+}
+
+void ScalarConverter::convert(std::string &value)
+{
+    set_type(value);
+    switch(type)
     {
-        
+    case CHAR:
+        set_char(value[0]);
+        break;
+    // case INT:
+    //     print_int(value[0]);
+    default:
+        break;
     }
+    
+}
+
+std::ostream& operator<<(std::ostream& o, ScalarConverter const & f)
+{
+    o << "char : " << f.print_char();
+    o << "int  : " << f.print_int();
+    return o;
 }
--- cpp6/ex00/ScalarConverter.hpp
+++ cpp6/ex00/ScalarConverter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 14:49:19 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/12/11 16:17:25 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/12/28 19:21:52 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 #include <iostream>
 #include <exception>
 #include <stdio.h>
+#include <ctype.h>
+#define CHAR 0
+#define INT 1
+#define FLOAT 2
+#define DOUBLE 3
 
 class ScalarConverter
 {
@@ -25,9 +30,23 @@ class ScalarConverter
     ScalarConverter& operator=(ScalarConverter const & fp);
     ScalarConverter(const ScalarConverter & fp);
     
+    int type;
+    bool isImposible;
+    char c;
+    int n;
+    float f;
+    double d;
+    
     public:
-    static void convert(std::string value);
-    int is_int(std::string value);
+    void convert(std::string &value);
+    bool is_int(std::string value);
+    void set_type(std::string &value);
+    bool is_char(std::string &value);
+    void set_char(char value);
+    void print_char();
+    void set_int(std::string &value);
+     void print_int();
 };
 
+std::ostream& operator<<(std::ostream& o, ScalarConverter const & f);
 #endif
