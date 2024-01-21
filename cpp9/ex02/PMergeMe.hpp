890aaa616b8f8f8e8e223a15cbe5450063912b9c
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2024-01-21 21:01:50 +0200

    working on ex02

--- cpp9/ex02/PMergeMe.hpp
+++ cpp9/ex02/PMergeMe.hpp
@@ -6,7 +6,23 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 17:20:04 by jpelaez-          #+#    #+#             */
-/*   Updated: 2024/01/16 17:20:04 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2024/01/21 20:48:16 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef P_MERGE_ME_HPP
+# define P_MERGE_ME_HPP
+
+#include <iostream>
+#include <exception>
+#include <fstream>
+#include <stdlib.h>
+#include <deque>
+#include <vector>
+
+class PMergeMe
+{
+
+};
+
+#endif
--- cpp9/ex02/main.cpp
+++ cpp9/ex02/main.cpp
@@ -6,7 +6,39 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 17:19:23 by jpelaez-          #+#    #+#             */
-/*   Updated: 2024/01/16 17:19:29 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2024/01/21 20:58:56 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PMergeMe.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        std::cout << "Error: Invalid number of arguments" << std::endl;
+        return (1);
+    }
+    for(int i = 1 ; i < argc; i++)
+    {
+        if(isdigit(argv[i][0]) == 0)
+        {
+            std::cout << "Error: Invalid argument" << std::endl;
+            return (1);
+        }
+    }
+    int number;
+    std::deque<int> dq;
+    for(int i = 1; i < argc; i++)
+    {
+        number = atoi(argv[i]);
+        if(number < 0)
+        {
+            std::cout << "Error: Negative number" << std::endl;
+            return (1);   
+        }
+        dq.push_back(number);
+    }
+    
+    
+}
